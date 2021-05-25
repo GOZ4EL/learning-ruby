588c8d17ab0315a928f057e77f5964769045b08b
GOZ4EL <hordemzerado@gmail.com>
2021-05-25 18:20:34 -0400

    startup project done

--- OOP/startup_project/lib/startup.rb
+++ OOP/startup_project/lib/startup.rb
@@ -23,4 +23,49 @@ class Startup
             false
         end
     end
+
+    def hire(employee_name, title)
+        raise "Invalid title" if !valid_title?(title)
+
+        @employees << Employee.new(employee_name, title)
+    end
+
+    def size 
+        @employees.length
+    end
+
+    def pay_employee(employee)
+        salary = @salaries[employee.title]
+
+        raise "Insufficent funds to pay this salary" if @funding < salary
+        
+        employee.pay(salary)
+        @funding -= salary
+    end
+
+    def payday
+        @employees.each { |employee| pay_employee(employee) }
+    end
+
+    def average_salary
+        total = 0
+        @employees.each { |employee| total += @salaries[employee.title] }
+
+        total / @employees.length
+    end
+
+    def close
+        @employees = []
+        @funding = 0
+    end
+
+    def acquire(startup)
+        @funding += startup.funding
+
+        startup.salaries.each { |key, val| @salaries[key] = val if !@salaries.key?(key) }
+
+        @employees.concat(startup.employees)
+        
+        startup.close
+    end
 end
